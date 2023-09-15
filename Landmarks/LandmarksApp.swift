cb17df782a05194fdd759f36c56709c162e1b0c7
Denis Bystruev <dbystruev@me.com>
2023-09-15 22:37:08 +0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Denis Bystruev on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
