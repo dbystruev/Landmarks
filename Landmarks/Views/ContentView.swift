8fb4cb95358a63cef77fb0d69092751607bfce2a
Denis Bystruev <dbystruev@me.com>
2023-09-16 15:31:19 +0400

    Building Lists and Navigation (ContentView)

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Denis Bystruev on 9/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (3rd generation)",
+                 "iPhone 14 Pro Max",
+                 "iPad (10th generation)",
+                 "My Mac"],
+                id: \.self) { deviceName in
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
